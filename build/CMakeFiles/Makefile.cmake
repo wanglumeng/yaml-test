16f98e9716121bb5ba3836c1f620467329557079
lumengwang <lumengwang@deepglint.com>
2020-07-29 19:53:59 +0800

    compile ok

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -42,5 +42,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/node.dir/DependInfo.cmake"
+  "CMakeFiles/read.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/readyaml.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/readyaml.dir/readyaml.cpp.o"
+  "readyaml.pdb"
+  "readyaml"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/readyaml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
